5257467f8c97e319608986496c5e7575ad108556
Mustapha Moshood <olawale3415@gmail.com>
2020-07-13 19:29:49 +0100

    Updates

---
+++ precache-manifest.fb65984d0314e95cec08019930c6bc97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b6146f42770948248ec9c3ffa97a7f",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "7104e7b9abbd2e2eeda4",
+    "url": "/quiz-app/static/css/main.09936ad1.chunk.css"
+  },
+  {
+    "revision": "ed9ea346e3ff48e375c0",
+    "url": "/quiz-app/static/js/2.4ad7820e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.4ad7820e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7104e7b9abbd2e2eeda4",
+    "url": "/quiz-app/static/js/main.6e53a478.chunk.js"
+  },
+  {
+    "revision": "915e3cb81322fb947610",
+    "url": "/quiz-app/static/js/runtime-main.6f0d185b.js"
+  }
+]);
